e068f4f95e93a742cf601bbe34f1b93b57e77b43
Ayush Shrivastava <ayushshrivastava@Ayushs-MacBook-Air-3.local>
2025-02-23 00:52:48 +0530

    Adding initial files

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # srip25_Asst
+
+Your task is to implement the following functions in the 'metrics.py' file:
+1. custom_accuracy_score
+2. custom_precision_score
+3. custom_recall_score
+4. custom_f1_score
+5. custom_confusion_matrix
+
+You are required to test the functions by running the 'ml_model.py' or 'ml_model.ipynb' file.
+When in doubt please contact the mentor/instructor.
---
+++ metrics.py
@@ -0,0 +1,70 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= Instructions =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+# In this assignment, you will be implementing custom metrics functions to evaluate the classification model.
+# The task is to use the created custom metrics functions and compare them with the sklearn's metrics functions.
+# You use the ml_model.py or ml_model.ipynb file to compare the metrics. 
+# When in doubt please reach out to the mentor and thet will help you.
+#
+# Reading material: 
+# 1. https://www.analyticsvidhya.com/blog/2021/07/metrics-to-evaluate-your-classification-model-to-take-the-right-decisions/
+# 2. https://youtu.be/Kdsp6soqA7o?si=xZYnBUb6Tk74tQEX
+#
+# Note: 
+# 1. Please do not change the function signature of the functions
+# 2. Use of AI tools is not allowed. Try to write the code on your own.
+#
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= Instructions =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+
+
+def custom_accuracy_score(y_true, y_pred):
+    """
+    This function calculates the accuracy score of the model
+    :param y_true: true values
+    :param y_pred: predicted values
+    :return: accuracy score
+    """
+    # Write your code here and remove the pass
+    pass
+
+def custom_precision_score(y_true, y_pred):
+    """
+    This function calculates the precision score of the model
+    :param y_true: true values
+    :param y_pred: predicted values
+    :return: precision score
+    """
+    # Write your code here and remove the pass
+    pass
+
+def custom_recall_score(y_true, y_pred):
+    """
+    This function calculates the recall score of the model
+    :param y_true: true values
+    :param y_pred: predicted values
+    :return: recall score
+    """
+    # Write your code here and remove the pass
+    pass
+
+def custom_f1_score(y_true, y_pred):
+    """
+    This function calculates the f1 score of the model
+    :param y_true: true values
+    :param y_pred: predicted values
+    :return: f1 score
+    """
+    # Write your code here and remove the pass
+    pass
+
+def plot_confusion_matrix(y_true, y_pred):
+    """
+    This function plots the confusion matrix
+    :param y_true: true values
+    :param y_pred: predicted values
+    :return: None
+    """
+    # Write your code here and remove the pass
+    pass
