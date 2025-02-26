e65735ada7c622d0eed729c4ef2407b9143f6cab
Aayush75 <aayushkuloor@gmail.com>
2025-02-26 19:56:00 +0530

    Fixed Confusion Matrix and Implemented Custom Functions
    Fixed the given confusion matrix and implemented the custom functions.

--- metrics.py
+++ metrics.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= Instructions =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
 # In this assignment, you will be implementing custom metrics functions to evaluate the classification model.
@@ -26,8 +27,9 @@ def custom_accuracy_score(y_true, y_pred):
     :param y_pred: predicted values
     :return: accuracy score
     """
-    # Write your code here and remove the pass
-    pass
+    true_vals_count = np.sum(y_pred == y_true)
+    return true_vals_count/len(y_true)       # len(y_true) will return the total number of elements in y_true, which is equal to the sum of the counts of all true values and false values
+    
 
 def custom_precision_score(y_true, y_pred):
     """
@@ -36,8 +38,18 @@ def custom_precision_score(y_true, y_pred):
     :param y_pred: predicted values
     :return: precision score
     """
-    # Write your code here and remove the pass
-    pass
+    classes = [0, 1]
+    precision_sum = 0
+    
+    for current_class in classes:
+        TP = np.sum((y_pred == current_class) & (y_true == current_class))
+        FP = np.sum((y_pred == current_class) & (y_true != current_class))
+        
+        if (TP + FP) != 0:
+            precision_sum += TP/(TP + FP)
+        
+    return precision_sum/len(classes)
+        
 
 def custom_recall_score(y_true, y_pred):
     """
@@ -46,8 +58,16 @@ def custom_recall_score(y_true, y_pred):
     :param y_pred: predicted values
     :return: recall score
     """
-    # Write your code here and remove the pass
-    pass
+    classes = [0, 1]
+    recall_sum = 0
+    
+    for current_class in classes:
+        TP = np.sum((y_pred == current_class) & (y_true == current_class))
+        FN = np.sum((y_pred != current_class) & (y_true == current_class))
+        if (TP + FN) != 0:
+            recall_sum += TP/(TP + FN)
+        
+    return recall_sum/len(classes)
 
 def custom_f1_score(y_true, y_pred):
     """
@@ -56,8 +76,26 @@ def custom_f1_score(y_true, y_pred):
     :param y_pred: predicted values
     :return: f1 score
     """
-    # Write your code here and remove the pass
-    pass
+    classes = [0, 1]
+    f1_sum = 0
+    
+    for current_class in classes:
+        TP = np.sum((y_pred == current_class) & (y_true == current_class))
+        FP = np.sum((y_pred == current_class) & (y_true != current_class))
+        FN = np.sum((y_pred != current_class) & (y_true == current_class))
+        
+        precision = 0
+        recall = 0
+        
+        if (TP + FP) > 0:
+            precision = TP / (TP + FP)
+        if (TP + FN) > 0:
+            recall = TP / (TP + FN)
+        
+        if (precision + recall) > 0:
+            f1_sum += 2 * precision * recall / (precision + recall)
+        
+    return f1_sum / len(classes)
 
 def plot_confusion_matrix(y_true, y_pred):
     """
@@ -66,5 +104,17 @@ def plot_confusion_matrix(y_true, y_pred):
     :param y_pred: predicted values
     :return: None
     """
-    # Write your code here and remove the pass
-    pass
+    TP = np.sum((y_pred == 1) & (y_true == 1))
+    TN = np.sum((y_pred == 0) & (y_true == 0))
+    FP = np.sum((y_pred == 1) & (y_true == 0))
+    FN = np.sum((y_pred == 0) & (y_true == 1))
+    
+    matrix = np.array([[TN, FP], [FN, TP]])
+    
+    sns.heatmap(matrix, cmap='Blues', annot=True, fmt='d')
+    plt.title('Custom Confusion Matrix')
+    plt.xlabel("Model Predicted Values")
+    plt.ylabel("Ground Truth Values")
+    plt.xticks(label=[0, 1])
+    plt.yticks(label=[0, 1])
+    plt.show()
